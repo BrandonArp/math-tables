0d421c0e8162806d53c8201062b41cbbd1743fdb
Brandon Arp <brandonarp@gmail.com>
2021-05-08 16:28:51 -0700

    add a field for pages generated

--- js/main.js
+++ js/main.js
@@ -37,6 +37,7 @@ function submitForm(e) {
   const num_max = document.getElementById("num_max").value;
   const denom_min = document.getElementById("den_min").value;
   const denom_max = document.getElementById("den_max").value;
+  const sheets = document.getElementById("sheets").value;
 
   doc.setFontSize(24);
 
@@ -68,39 +69,44 @@ function submitForm(e) {
   shuffle(numbers);
   let index = 0;
 
-  for (let j = 0; j < rows; j++) {
-    for (let i = 0; i < columns; i++) {
-      if (index >= numbers.length) {
-        shuffle(numbers);
-        index = 0;
+  for (let sheet = 0; sheet < sheets; sheet++) {
+    for (let j = 0; j < rows; j++) {
+      for (let i = 0; i < columns; i++) {
+        if (index >= numbers.length) {
+          shuffle(numbers);
+          index = 0;
+        }
+
+        let num = numbers[index];
+        index++;
+
+        let decoded = decode(num);
+        let numerator = decoded[0];
+        let denominator = decoded[1];
+
+        let num_text = numerator.toString();
+        let den_text = "x " + denominator.toString();
+        let num_size = doc.getTextDimensions(num_text);
+        let den_size = doc.getTextDimensions(den_text);
+
+        let text_width = Math.max(num_size.w, den_size.w);
+        let text_height = num_size.h + den_size.h;
+
+
+        let xCenter = (i * xFactor) + margin;
+        let yCenter = (j * yFactor) + margin;
+        let xOffset = text_width / 2;
+        let yOffset = text_height / 2;
+        doc.text(num_text, xCenter + xOffset - num_size.w, yCenter - yOffset);
+        doc.text(den_text, xCenter + xOffset - den_size.w, yCenter - yOffset + num_size.h);
+        doc.setLineWidth(0.02);
+        // doc.rect(xCenter - xOffset, yCenter - yOffset, text_width, text_height);
+        doc.line(xCenter - (xOffset * 1.08), yCenter + yOffset - den_size.h + 0.05, xCenter + (xOffset * 1.08), yCenter + yOffset - den_size.h + 0.05);
+
       }
-
-      let num = numbers[index];
-      index++;
-
-      let decoded = decode(num);
-      let numerator = decoded[0];
-      let denominator = decoded[1];
-
-      let num_text = numerator.toString();
-      let den_text = "x " + denominator.toString();
-      let num_size = doc.getTextDimensions(num_text);
-      let den_size = doc.getTextDimensions(den_text);
-
-      let text_width = Math.max(num_size.w, den_size.w);
-      let text_height = num_size.h +  den_size.h;
-
-
-      let xCenter = (i * xFactor) + margin;
-      let yCenter = (j * yFactor) + margin;
-      let xOffset = text_width / 2;
-      let yOffset = text_height / 2;
-      doc.text(num_text, xCenter + xOffset - num_size.w, yCenter - yOffset);
-      doc.text(den_text, xCenter + xOffset - den_size.w, yCenter - yOffset + num_size.h);
-      doc.setLineWidth(0.02);
-      // doc.rect(xCenter - xOffset, yCenter - yOffset, text_width, text_height);
-      doc.line(xCenter - (xOffset * 1.08), yCenter + yOffset - den_size.h + 0.05, xCenter + (xOffset * 1.08), yCenter + yOffset - den_size.h + 0.05);
-
+    }
+    if (sheet < sheets - 1) {
+      doc.addPage();
     }
   }
 
